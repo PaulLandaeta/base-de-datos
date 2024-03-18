839f8c375414255dc116b3a5bd319069a101aa7b
Paul Landaeta <paulwilkerlf@gmail.com>
2024-03-18 13:19:45 -0400

    feat: dates

--- DQL/dates.sql
+++ DQL/dates.sql
@@ -35,3 +35,48 @@ from songs;
 SELECT YEAR(fecha);
 SELECT MONTH(fecha);
 SELECT DAY(fecha);
+
+
+-- Obtener los usuarios que se registraron 3 meses atras 
+
+select DATE_SUB(CURDATE(), INTERVAL 3 MONTH) ;
+SELECT * 
+from users
+where created_at >= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)  ;
+
+
+-- mostrar los usuarios con mas de 30 años 
+
+SELECT * 
+from users
+where birthrate <= DATE_SUB(CURDATE(), INTERVAL 30 YEAR);
+
+
+SELECT * 
+from users 
+where year(curdate()) - year(birthrate) < 30;
+
+SELECT username, email, year(now()) - year(birthrate) as edad 
+from users
+where year(now()) - year(birthrate) >= 30;
+
+
+-- Cuanto tiempo transcurrio desde que te registraste hasta el dia de hoy
+
+SELECT TIMESTAMPDIFF(TIPO, date1, date2 );
+
+-- Mostrar los emails de los usuarios que esten pronto a vencer
+-- (10 dias) su plan de 3 meses,
+--  asumir que cada mes tiene solo 30 dias 
+
+SELECT count(*)
+FROM users
+WHERE timestampdiff(day, created_at, now()) >= 80 
+and timestampdiff(day, created_at, now()) <= 90;
+
+SELECT count(*)
+FROM (
+SELECT email, TIMESTAMPDIFF(DAY, created_at, now()) as Dias
+FROM users
+) as tabla1
+WHERE Dias-90 <= 0 and Dias >= -10;
